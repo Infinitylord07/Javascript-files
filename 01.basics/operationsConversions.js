fea024f803dcf9185701a5b504a60c144a0072d8
saurabh-workshere <skyscraper.boy07@gmail.com>
2026-04-02 20:25:01 +0000

    operations conversion done

---
+++ 01.basics/operationsConversions.js
@@ -0,0 +1,14 @@
+let value=3
+let negavalue=-value
+console.log(negavalue)
+
+let x=24
+//let y=x++
+let y=++x
+
+
+//console.log(y)
+
+let gamecounter=1000
+++gamecounter;
+console.log(gamecounter)
