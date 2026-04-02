48635fd4a6eb8ecf1bd61b01d1b357484230812d
saurabh-workshere <skyscraper.boy07@gmail.com>
2026-04-02 19:49:27 +0000

    conversion of datatypes

---
+++ 01.basics/02.datatypes.js
@@ -0,0 +1,21 @@
+"use strict" ;
+let fordNo= "undefined"
+//console.log(typeof fordNo)
+//console.log(typeof (fordNo))
+
+let loggedIn= Number(fordNo)
+//console.log(typeof loggedIn)
+
+let somenumber=33
+//console.log(typeof somenumber)
+
+let stringnumber=String(somenumber)
+//console.log(typeof stringnumber)
+
+let booleanfor= 1
+console.log(typeof booleanfor)
+
+
+
+
+
