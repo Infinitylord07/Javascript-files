a645b6defccbeece64fb62c5745661d688695d24
saurabh-workshere <skyscraper.boy07@gmail.com>
2026-04-04 21:31:27 +0000

    learn about memory allocation in stack and heap

---
+++ 01.basics/memory_basic.js
@@ -0,0 +1,21 @@
+// primitive jate hai stack me
+// non-primitive jate hai me heap me
+
+/*
+let myBranchName= "information technology"
+let myCourse="btech"
+let anothername=myBranchName
+anothername="computer science and engineering"
+console.log(myBranchName)
+console.log(anothername) */
+
+let user1={
+    email:"user1@gmail.com",
+    upi:"Saurabh@ybl"
+}
+let user2=user1
+user2.upi="Janhavi@ybl"
+
+
+console.log(user1)
+console.log(user2)
