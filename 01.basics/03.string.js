0063388b405d2e554e6b421e06b5aa7382767458
saurabh-workshere <skyscraper.boy07@gmail.com>
2026-04-06 07:31:27 +0000

    strings commands applied

--- 01.basics/03.string.js
+++ 01.basics/03.string.js
@@ -1,4 +1,57 @@
 const myBranchName= "Noida"
 const myShift="Night"
 
-console.log(`so my office branch is ${myBranchName} and my working shift is ${myShift}`)
+//console.log(`so my office branch is ${myBranchName} and my working shift is ${myShift}`)
+
+let GameName="Game-Of-Thrones"
+//console.log(GameName);
+
+urban=GameName.substring(0,7);
+//console.log(urban);
+
+/* const a = "a";
+const b = "b";
+if (a < b) {
+  // true
+  console.log(`${a} is less than ${b}`);
+} else if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} else {
+  console.log(`${a} and ${b} are equal.`);
+}
+  */
+
+/*const opd="vartman"[1];
+console.log(opd)
+
+const apd="vartman".charAt(3);
+console.log(apd) */
+
+//console.log(GameName.charAt(5));
+
+//console.log(GameName.indexOf('O'));
+
+
+/*
+
+const Newstring="    saurabh loves janhavi      ";
+console.log(Newstring)
+console.log(Newstring.trim()) // trim removes extra spaces
+
+*/
+
+
+const url="https.\\saurabhffirstwebsite.com\saurabh%20kushwaha"
+
+/*console.log(url.replace('ffirst','loves'));
+
+console.log(url.includes('website'));
+console.log(url.includes('bouncer')); */
+
+console.log(GameName.split('o'))
+
+
+
+
+
+
