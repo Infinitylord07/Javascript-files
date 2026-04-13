889d0cd75f263d9bcef3ceddcdf0afc4cb265970
saurabh-workshere <skyscraper.boy07@gmail.com>
2026-04-13 12:27:43 +0000

    learn different types of loop

---
+++ 05.Iterations/one.js
@@ -0,0 +1,36 @@
+
+//for(i=0;i<8;i++){
+    //console.log(i);}
+
+/* for (let i = 0; i <=10; i++) {
+     element = i;
+     if(element==7){
+        console.log("iteration done")
+     }
+     console.log(element)
+
+    
+}   */
+
+
+    /*
+     for (let i = 1; i < 5; i++) {
+        //console.log(`inner loop for ${i}`)
+        for (let j = 1; j <5; j++) {
+            //console.log(`outer loop for ${j}`)
+            let element=i*j
+            console.log(`${i} * ${j} = ${element} `)
+            
+            
+        }
+        
+     }
+        */
+
+
+     let myArray=["IRON MAN","THOR","WANDA"]
+     for (let index = 0; index < myArray.length; index++) {
+        const element = myArray[index];
+        console.log(element)
+        
+     }
---
+++ 05.Iterations/two.js
@@ -0,0 +1,24 @@
+// we will learn about while loop
+//let i=0
+//while(i<10){
+    //console.log(i)
+   // i=i+2}
+
+/*
+   let myArray=["superman","homelander","thor"]
+arr=0
+while(arr<myArray.length){
+    console.log(myArray[arr])
+    arr=arr+2
+}
+
+*/
+
+let score=4
+
+do{
+    console.log(`score is ${score}`)
+    score++
+
+}while(score<3)
+
